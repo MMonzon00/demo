a06459c8b099f8afec6deec98e6af2e2904d4b2d
Martin Monzon <martinmonzon00@gmail.com>
2023-01-25 14:36:24 -0300

    Organization update

--- src/main/java/lp3/backend/Lp3MonzonApplication.java
+++ src/main/java/lp3/backend/Lp3MonzonApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class  DemoApplication {
+public class Lp3MonzonApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(Lp3MonzonApplication.class, args);
 	}
 
 }
---
+++ src/main/java/lp3/backend/model/Product.java
@@ -0,0 +1,3 @@
+//package lp3.backend.model;
+
+//public class Product { }
