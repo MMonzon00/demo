c7ca453449704f4083bc7e29929361c3979d6fa7
c4russian <martinmonzon00@gmail.com>
2023-01-25 21:22:13 -0300

    New classes added

--- src/main/java/lp3/backend/model/Organization.java
+++ src/main/java/lp3/backend/model/Organization.java
@@ -1,10 +1,6 @@
 package lp3.backend.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 
 import java.util.UUID;
 
